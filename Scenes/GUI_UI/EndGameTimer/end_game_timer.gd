695166f75e4141ee89d317711bc25742eec5b0b5
Beicker Zambrano <beicker998@gmail.com>
2023-08-30 13:30:49 -0500

    Añadir escena para el Timer del First World

---
+++ Scenes/GUI_UI/EndGameTimer/end_game_timer.gd
@@ -0,0 +1,27 @@
+extends Node2D
+signal timeout
+
+@export var minutes_time: int
+var current_seconds: int
+ 
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	current_seconds = minutes_time * 60
+	$Label.text = "0{minutes}:00".format({"minutes": minutes_time})
+
+func _on_timer_timeout():
+	if current_seconds > 0:
+		current_seconds -= 1
+		var minutes = current_seconds / 60
+		var seconds = current_seconds % 60
+		var seconds_template = "{seconds}"
+		var minutes_template = "{minutes}"
+		if seconds < 10:
+			seconds_template = "0{seconds}"
+		if minutes < 10:
+			minutes_template = "0{minutes}"
+		var label_template = minutes_template + ":" + seconds_template
+		$Label.text = label_template.format({"minutes": minutes, "seconds": seconds})
+		
+	if current_seconds == 0:
+		emit_signal("timeout")
